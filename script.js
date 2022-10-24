1fffd7303d8699af44bde2d49ea81fbc70db5e41
mohammedalgamal <mohammedalgamal95@gmail.com>
2022-10-24 10:22:46 +0200

    Make the calculation functions

---
+++ script.js
@@ -0,0 +1,18 @@
+function add(num1, num2) {
+    return num1 + num2;
+};
+
+function sub(num1, num2) {
+    return num1 - num2;
+};
+
+function mul(num1, num2) {
+    return num1 * num2;
+};
+
+function div(num1, num2) {
+    if (num2 === 0){
+        return 'really? :)';
+    }
+    return num1 + num2;
+};
